8bd0347f42e76725f6f58e79774f848e02daf198
myy <1838278723@qq.com>
2023-12-23 01:43:17 +0800

    Changes

---
+++ coupon/src/test/java/com/myy/test/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myy.test.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
